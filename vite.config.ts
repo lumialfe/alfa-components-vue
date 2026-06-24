d16f845e3627ad3d810254ebf81dc47b67c5f254
lumialfe <lumialfe@hotmail.com>
2026-06-24 11:23:54 +0200

    dependencies: remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 import path from 'node:path';
@@ -12,7 +11,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(dirname, './src'),
